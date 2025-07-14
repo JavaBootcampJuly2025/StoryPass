cf2c27c23043264e9199e45a12a060d97d60c973
Viktorija <vikula778@gmail.com>
2025-07-14 12:43:42 +0300

    Initial project setup

---
+++ storypass/src/main/java/com/storypass/storypass/StorypassApplication.java
@@ -0,0 +1,13 @@
+package com.storypass.storypass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorypassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorypassApplication.class, args);
+	}
+
+}
---
+++ storypass/src/test/java/com/storypass/storypass/StorypassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storypass.storypass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorypassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
