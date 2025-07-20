d4e73a2ddab5c2d56e53cbb3c93c2f1bf18da951
Valdis Arelis <arelisvaldis@gmail.com>
2025-07-20 15:01:40 +0300

    Added leave function #29. Fixed some exception stuff and improved the user model for ease of use.

--- storypass/src/main/java/com/storypass/storypass/model/User.java
+++ storypass/src/main/java/com/storypass/storypass/model/User.java
@@ -60,4 +60,20 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+
+    // These two overrides make it so .contains(user) and .remove(user) works properly.
+    // Not fully sure how (made by AI), but it works
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id != null && id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
