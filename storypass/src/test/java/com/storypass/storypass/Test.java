a1a33cbfd2dc5b15abd483828ca23f3a65b4d4bc
Valdis Arelis <arelisvaldis@gmail.com>
2025-07-22 21:41:40 +0300

    It doesn't work

---
+++ storypass/src/test/java/com/storypass/storypass/Test.java
@@ -0,0 +1,14 @@
+package com.storypass.storypass;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Testing {
+
+    @org.junit.jupiter.api.Test
+    void test() {
+        Assertions.assertEquals(1, 1);
+    }
+
+}
