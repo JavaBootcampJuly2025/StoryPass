a4ef6b7cdd7faeb23ed9a99a0b1058047a5dc339
Andrejs Zmakins <andrejs.zmakins@vodafone.com>
2025-07-24 20:08:23 +0300

    StoryPass 12 proper unit test for PDF export

--- storypass/src/test/java/com/storypass/storypass/PdfExportTest.java
+++ storypass/src/test/java/com/storypass/storypass/PdfExportTest.java
@@ -1,29 +1,54 @@
 package com.storypass.storypass;
 
+import com.storypass.storypass.dto.FullStoryDto;
+import com.storypass.storypass.dto.StoryLineDto;
 import com.storypass.storypass.service.PdfExportService;
+import com.storypass.storypass.service.StoryService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.util.List;
 
-@SpringBootTest(classes = PdfExportService.class)
 public class PdfExportTest {
 
-    @Autowired
-    PdfExportService pdfExportService;
+    private PdfExportService pdfExportService;
+
+    @BeforeEach
+    void setUp() {
+        var storyService = Mockito.mock(StoryService.class);
+
+        Mockito.when(storyService.getFullStoryById(Mockito.anyLong())).thenReturn(
+                new FullStoryDto("Humpty Dumpty",
+                        List.of("John Doe", "Ivan Petrov", "Max Mustermann"),
+                        List.of(
+                                new StoryLineDto("Humpty Dumpty sat on a wall.", "John"),
+                                new StoryLineDto("Humpty Dumpty had a great fall.", "Ivan"),
+                                new StoryLineDto("All the king's horses and all the king's men", "Max"),
+                                new StoryLineDto("Couldn't put Humpty together again.", "John")
+                        ))
+        );
+
+        pdfExportService = new PdfExportService(storyService);
+    }
 
     @Test
-    void test() throws IOException {
-        byte[] data = pdfExportService.generatePdfForStory(1L);
+    void testSuccess() {
+        Assertions.assertDoesNotThrow(() -> {
+                byte[] data = pdfExportService.generatePdfForStory(1L);
 
-        File file = new File("StoryPass.pdf");
+                File file
+                        //= new File("HumptyDumpty.pdf"); // Use this is you want to get the file locally!
+                        = File.createTempFile("test", "HumptyDumpty.pdf");
 
-        var fileOutputStream = new FileOutputStream(file);
-        fileOutputStream.write(data);
-        fileOutputStream.flush();
-        fileOutputStream.close();
+                var fileOutputStream = new FileOutputStream(file);
+                fileOutputStream.write(data);
+                fileOutputStream.flush();
+                fileOutputStream.close();
+            }
+        );
     }
 }
